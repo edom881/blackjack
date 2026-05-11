c93b66a1b8db8b00bcd697315fad5b1ecc188293
edomg <edomgezahegn54@gmail.com>
2026-05-11 13:17:09 -0500

    Create blackjack project and prompt for player names

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,23 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to Blackjack!");
+
+        System.out.print("Enter player 1 name: ");
+        String player1Name = scanner.nextLine();
+
+        System.out.print("Enter player 2 name: ");
+        String player2Name = scanner.nextLine();
+
+        System.out.println();
+        System.out.println("Player 1: " + player1Name);
+        System.out.println("Player 2: " + player2Name);
+    }
+}
+
